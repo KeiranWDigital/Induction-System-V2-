1b2a5c6111f92ff5ff20081e3a1d1a396211ff59
Keiran Wilkinson <keiran@keiranwilkinson.com>
2020-05-21 22:36:33 +0100

    Add project files.

---
+++ Controllers/InductionController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class InductionController : Controller
+    {
+        // GET: Induction
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        // GET: NewInduction
+        public ActionResult NewInduction()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NewInduction(InductionModel Induction)
+        { int InductionID = Induction.ID;
+            String InductionName = Induction.Name;
+            String InductionDescription = Induction.Description;
+            
+            return View();
+        }
+    }
+}
